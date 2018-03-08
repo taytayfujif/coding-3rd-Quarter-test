ad6b7b30190342e270159d685e9851c106a32628
taytayfujif <31020217+taytayfujif@users.noreply.github.com>
2018-03-07 14:13:02 -1000

    Final commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+<script type="text/javascript">
+	function colors= (blue, pink, green,orange) {
+		var color1= "blue";
+		var color2= "pink";
+		var color3= "green";
+		var color4= "orange";
+			var color = [
+			 "blue",
+			 "pink"
+			 "green"
+			 "orange"
+			 ]
+			var = 3
+		 var color3= 3 
+		 	var color = [
+		 	"blue"
+		 	"pink"
+		 	   3
+		 	"orange"
+		 	]
+
+	return; 
+	}
+ document.getElementById("fourth").innerHTML= function colors 
+</script>
